2814b5507ca86ae926873c40af0273591f18a4e8
ZenBotTX9000 <rstdare@gmail.com>
2025-06-08 13:18:59 +0200

    Add files via upload

---
+++ app/api/page.tsx
@@ -0,0 +1,9 @@
+import { MainChatInterface } from "@/components/main-chat-interface";
+
+export default function Home() {
+  return (
+    <main className="w-full h-screen overflow-hidden flex flex-col">
+      <MainChatInterface />
+    </main>
+  );
+}
