7cea0e117975d310b1a1b7d548dc7a49e17a59bb
ZenBotTX9000 <rstdare@gmail.com>
2025-06-08 13:20:10 +0200

    Delete app/api/page.tsx

--- app/api/page.tsx
+++
@@ -1,9 +0,0 @@
-import { MainChatInterface } from "@/components/main-chat-interface";
-
-export default function Home() {
-  return (
-    <main className="w-full h-screen overflow-hidden flex flex-col">
-      <MainChatInterface />
-    </main>
-  );
-}
